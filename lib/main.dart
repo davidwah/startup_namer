0b519f8c99fe7a342bfc90d72a6031feb85123a1
davidwah <davidwahyuyu@gmail.com>
2022-04-21 06:27:15 +0700

    change theme UI

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white54,
+          foregroundColor: Colors.black,
+        ),
+      ),
       home: RandomWords(),
     );
   }
